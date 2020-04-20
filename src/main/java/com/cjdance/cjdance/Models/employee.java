d183ffc5e5244779940edd08e8bab85d4c6dc668
Cindy <ctnguyen78@uh.edu>
2020-04-20 16:36:21 -0500

    create classes

--- src/main/java/com/cjdance/cjdance/Models/employee.java
+++ src/main/java/com/cjdance/cjdance/Models/employee.java
@@ -6,13 +6,45 @@ import javax.persistence.;
 @Table(name = "employee")
 public class employee {
     @Id
-    @Column(name = "id")
-    private String id;
-    @Column(name = "firstname")
-    private String firstName;
-    @Column(name = "lastname")
-    private String lastName;
-    @Column(name = "isactive")
-    private boolean isActive;
+    @Column(name = "empid")
+    private String empid;
+    @Column(name = "empfname")
+    private String empfname;
+    @Column(name = "emplname")
+    private String emplname;
+    @Column(name = "empphone")
+    private String empphone;
+    @Column(name = "empemail")
+    private String empemail;
+    @Column(name = "isinstructor")
+    private boolean isinstructor;
+
+    public employee(){
+    }
+
+    public employee(String empid, String empfname, String emplname, String empphone, String empemail, boolean isinstructor){
+        this.empid = empid;
+        this.empfname = empfname;
+        this.emplname = emplname;
+        this.empphone = empphone;
+        this.empemail = empemail;
+        this.isinstructor = isinstructor;
+    }
+
+    public String getempid() {
+        return empid;
+    }
+
+    public void setempid(String empid) {
+        this.empid = empid;
+    }
+
+    public String getempfname() {
+        return empfname;
+    }
+
+    public void setEmpfname(String empid) {
+        this.empid = empid;
+    }
 
 }
